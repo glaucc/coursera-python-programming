5e181c55fdb57762f64057b93a006cf711599379
Puffy <88508824+PufflyMan@users.noreply.github.com>
2022-07-29 14:18:12 +0400

    couple config

--- week3/proc-func/maps_filters.py
+++ week3/proc-func/maps_filters.py
@@ -8,3 +8,11 @@ map_coffee = map(find_coffee, menu)
 print(map_coffee)
 for x in map_coffee:
     print(x)
+
+
+# a = [[96], [69]]
+
+# x = ''.join(list(map(str, a)))
+# print(list(map(str, a)))
+# print(type(x))
+# print(x)
